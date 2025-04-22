2277ea5972707fbcc462ec11c8de7cc3afba975a
Mialy RATSIMBAZAFY <mialy.ratsimbazafy@gmail.com>
2025-04-22 14:38:58 +0200

    update and improving VoiceAssistant feature

--- src/features/community/pages/CommunityPage.tsx
+++ src/features/community/pages/CommunityPage.tsx
@@ -1,1 +1,14 @@
-export default function CommunityPage() { return <div className="p-8 text-xl">Communauté 🤝</div>; }
+// features/community/pages/CommunityPage.tsx
+import { Community } from "../../../components/Community/Community";
+import { VoiceAssistant } from "../../../components/Community/VoiceAssistant";
+
+export default function CommunityPage() {
+  return (
+    <div className="community-container">
+      <Community /> {/* Ton composant existant */}
+      <div className="voice-assistant-container fixed bottom-6 right-6 z-50">
+        <VoiceAssistant /> {/* 👈 Ajouté ici */}
+      </div>
+    </div>
+  );
+}
